483a75e0f056147e18a4241e675476e2eb3c61f5
teodora <teamikic18@gmail.com>
2026-04-04 21:31:47 +0200

    feat: implemented KartaService

--- src/main/java/com/busticket/app/repository/KartaRepository.java
+++ src/main/java/com/busticket/app/repository/KartaRepository.java
@@ -4,6 +4,11 @@ import com.busticket.app.model.entity.Karta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface KartaRepository extends JpaRepository<Karta,Long> {
+
+    List<Karta> findAllByRezervacijaId(Long rezervacijaId);
+    List<Karta> findAllByPutovanjeId(Long putovanjeId);
 }
