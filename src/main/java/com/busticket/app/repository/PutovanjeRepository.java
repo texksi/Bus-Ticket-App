432f049258ec82b2dca728a3e375616cfa5fc0ae
teodora <teamikic18@gmail.com>
2026-04-04 22:31:53 +0200

    feat: implemented PutovanjeService

--- src/main/java/com/busticket/app/repository/PutovanjeRepository.java
+++ src/main/java/com/busticket/app/repository/PutovanjeRepository.java
@@ -4,6 +4,10 @@ import com.busticket.app.model.entity.Putovanje;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PutovanjeRepository extends JpaRepository<Putovanje,Long> {
+
+    List<Putovanje> findAllByKompanijaId(Long kompanijaId);
 }
