4142a7a1dd00fcce5516925587ebd13984e80b20
teodora <teamikic18@gmail.com>
2026-04-13 14:39:22 +0200

    feat: added dtos for Rezervacija

---
+++ src/main/java/com/busticket/app/model/dto/ResponseDTOs/RezervacijaResponseDTO.java
@@ -0,0 +1,21 @@
+package com.busticket.app.model.dto.ResponseDTOs;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RezervacijaResponseDTO {
+
+    private Long id;
+    private LocalDateTime datumKreiranja;
+    private double ukupanIznos;
+    private String nacinPlacanja;
+    private String status;
+    private Long korisnikId;
+
+}
