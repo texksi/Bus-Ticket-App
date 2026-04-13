78e21f7319ca99ed7bb0da08ec7529a556e103ff
teodora <teamikic18@gmail.com>
2026-04-13 18:47:59 +0200

    fix: refactored PutovanjeService to use mappers and dtos

--- src/main/java/com/busticket/app/service/PutovanjeService.java
+++ src/main/java/com/busticket/app/service/PutovanjeService.java
@@ -1,8 +1,15 @@
 package com.busticket.app.service;
 
 import com.busticket.app.exceptions.EntityNotFoundException;
+import com.busticket.app.mapper.PutovanjeMapper;
+import com.busticket.app.model.dto.RequestDTOs.PutovanjeRequestDTO;
+import com.busticket.app.model.dto.ResponseDTOs.PutovanjeResponseDTO;
+import com.busticket.app.model.entity.Kompanija;
 import com.busticket.app.model.entity.Putovanje;
+import com.busticket.app.model.entity.Vozilo;
+import com.busticket.app.repository.KompanijaRepository;
 import com.busticket.app.repository.PutovanjeRepository;
+import com.busticket.app.repository.VoziloRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,35 +21,55 @@ import java.util.List;
 public class PutovanjeService {
 
     private final PutovanjeRepository putovanjeRepository;
+    private final PutovanjeMapper putovanjeMapper;
+    private final VoziloRepository voziloRepository;
+    private final KompanijaRepository kompanijaRepository;
 
-    public Putovanje getPutovanjeById(Long id){
-        return putovanjeRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Putovanje nije pronadjeno"));
+    public PutovanjeResponseDTO getPutovanjeById(Long id) {
+        Putovanje putovanje = putovanjeRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Putovanje nije pronadjeno"));
+        return putovanjeMapper.toResponse(putovanje);
     }
 
-    public List<Putovanje> getAllPutovanja(){
-        return putovanjeRepository.findAll();
+    public List<PutovanjeResponseDTO> getAllPutovanja() {
+        List<Putovanje> putovanja = putovanjeRepository.findAll();
+        return putovanja.stream().map(putovanjeMapper::toResponse).toList();
     }
 
-    public Putovanje createPutovanje(Putovanje putovanje){
-        return putovanjeRepository.save(putovanje);
+    public PutovanjeResponseDTO createPutovanje(PutovanjeRequestDTO newPutovanje) {
+        Putovanje putovanje = putovanjeMapper.toEntity(newPutovanje);
+        Vozilo vozilo = voziloRepository.findById(newPutovanje.getVoziloId()).orElseThrow(
+                () -> new EntityNotFoundException("Vozilo nije pronadjeno")
+        );
+        Kompanija kompanija = kompanijaRepository.findById(newPutovanje.getKompanijaId()).orElseThrow(
+                () -> new EntityNotFoundException("Kompanija nije pronadjena")
+        );
+        putovanje.setVozilo(vozilo);
+        putovanje.setKompanija(kompanija);
+        Putovanje saved = putovanjeRepository.save(putovanje);
+        return putovanjeMapper.toResponse(saved);
     }
 
-    public Putovanje updatePutovanje(Long id, String polaziste, String odrediste, LocalDateTime vremePolaska, LocalDateTime vremeDolaska, double osnovnaCena){
-        Putovanje savedPutovanje = getPutovanjeById(id);
+    public PutovanjeResponseDTO updatePutovanje(Long id, String polaziste, String odrediste, LocalDateTime vremePolaska, LocalDateTime vremeDolaska, double osnovnaCena) {
+        Putovanje savedPutovanje = putovanjeRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Putovanje nije pronadjeno")
+        );
         savedPutovanje.setPolaziste(polaziste);
         savedPutovanje.setOdrediste(odrediste);
         savedPutovanje.setVremePolaska(vremePolaska);
         savedPutovanje.setVremeDolaska(vremeDolaska);
         savedPutovanje.setOsnovnaCena(osnovnaCena);
-        return putovanjeRepository.save(savedPutovanje);
+        Putovanje putovanje = putovanjeRepository.save(savedPutovanje);
+        return putovanjeMapper.toResponse(putovanje);
     }
 
-    public void deletePutovanje(Long id){
+    public void deletePutovanje(Long id) {
         putovanjeRepository.deleteById(id);
     }
 
-    public List<Putovanje> getPutovanjaByKompanija(Long kompanijaId){
-        return putovanjeRepository.findAllByKompanijaId(kompanijaId);
+    public List<PutovanjeResponseDTO> getPutovanjaByKompanija(Long kompanijaId) {
+        List<Putovanje> putovanja = putovanjeRepository.findAllByKompanijaId(kompanijaId);
+        return putovanja.stream().map(putovanjeMapper::toResponse).toList();
     }
 
 }
