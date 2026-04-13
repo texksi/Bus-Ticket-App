b9891236bf3b414da57e8f7caf530b0c9a162e76
teodora <teamikic18@gmail.com>
2026-04-13 14:47:33 +0200

    feat: added dtos for Kompanija

---
+++ src/main/java/com/busticket/app/model/dto/RequestDTOs/KompanijaRequestDTO.java
@@ -0,0 +1,15 @@
+package com.busticket.app.model.dto.RequestDTOs;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class KompanijaRequestDTO {
+
+    private String naziv;
+    private String kontakt;
+
+}
