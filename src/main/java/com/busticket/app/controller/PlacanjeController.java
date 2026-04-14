502fb9202b26f27753a0923d3a3d6a1284659201
teodora <teamikic18@gmail.com>
2026-04-14 13:02:04 +0200

    fix: refactored Placanje,Stripe and Ocena controller to use ResponseEntity and dtos

--- src/main/java/com/busticket/app/controller/PlacanjeController.java
+++ src/main/java/com/busticket/app/controller/PlacanjeController.java
@@ -1,8 +1,9 @@
 package com.busticket.app.controller;
 
-import com.busticket.app.model.entity.Placanje;
+import com.busticket.app.model.dto.ResponseDTOs.PlacanjeResponseDTO;
 import com.busticket.app.service.PlacanjeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +17,12 @@ public class PlacanjeController {
     private final PlacanjeService placanjeService;
 
     @GetMapping("/api/placanja/{id}")
-    public Placanje getPlacanjeById(@PathVariable Long id){
-        return  placanjeService.getPlacanjeById(id);
+    public ResponseEntity<PlacanjeResponseDTO> getPlacanjeById(@PathVariable Long id){
+        return  ResponseEntity.ok(placanjeService.getPlacanjeById(id));
     }
 
     @GetMapping("/api/placanja/rezervacija/{id}")
-    public List<Placanje> getPlacanjaForRezervacija(@PathVariable Long id){
-        return placanjeService.getPlacanjaForRezervacija(id);
+    public ResponseEntity<List<PlacanjeResponseDTO>> getPlacanjaForRezervacija(@PathVariable Long id){
+        return ResponseEntity.ok(placanjeService.getPlacanjaForRezervacija(id));
     }
 }
