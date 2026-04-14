5760a5b709c722d8d5b05683b38df160969d7774
teodora <teamikic18@gmail.com>
2026-04-14 14:35:52 +0200

    fix: refactored Rezervacija and Vozilo  unit tests with new mocks and dtos

--- src/test/java/com/busticket/app/serviceTests/VoziloServiceTest.java
+++ src/test/java/com/busticket/app/serviceTests/VoziloServiceTest.java
@@ -1,8 +1,12 @@
 package com.busticket.app.serviceTests;
 
 import com.busticket.app.exceptions.EntityAlreadyExistsException;
+import com.busticket.app.mapper.VoziloMapper;
+import com.busticket.app.model.dto.RequestDTOs.VoziloRequestDTO;
+import com.busticket.app.model.dto.ResponseDTOs.VoziloResponseDTO;
 import com.busticket.app.model.entity.Kompanija;
 import com.busticket.app.model.entity.Vozilo;
+import com.busticket.app.repository.KompanijaRepository;
 import com.busticket.app.repository.VoziloRepository;
 import com.busticket.app.service.VoziloService;
 import org.assertj.core.api.Assertions;
@@ -24,6 +28,10 @@ public class VoziloServiceTest {
 
     @Mock
     private VoziloRepository voziloRepository;
+    @Mock
+    private VoziloMapper voziloMapper;
+    @Mock
+    private KompanijaRepository kompanijaRepository;
     @InjectMocks
     private VoziloService voziloService;
     private Kompanija savedKompanija;
@@ -50,7 +58,8 @@ public class VoziloServiceTest {
     public void getVoziloById_Success() {
         Vozilo vozilo = builderVozilo();
         when(voziloRepository.findById(1L)).thenReturn(Optional.of(vozilo));
-        Vozilo found = voziloService.getVoziloById(1L);
+        when(voziloMapper.toResponse(vozilo)).thenReturn(new VoziloResponseDTO());
+        VoziloResponseDTO found = voziloService.getVoziloById(1L);
         Assertions.assertThat(found).isNotNull();
     }
 
@@ -58,23 +67,40 @@ public class VoziloServiceTest {
     public void getAllVozilaForKompanija_Success() {
         Vozilo vozilo = builderVozilo();
         when(voziloRepository.findAllByKompanijaId(vozilo.getKompanija().getId())).thenReturn(List.of(vozilo));
-        List<Vozilo> all = voziloService.getAllVozilaForKompanija(vozilo.getKompanija().getId());
+        when(voziloMapper.toResponse(vozilo)).thenReturn(new VoziloResponseDTO());
+        List<VoziloResponseDTO> all = voziloService.getAllVozilaForKompanija(vozilo.getKompanija().getId());
         Assertions.assertThat(all).hasSize(1);
     }
 
     @Test
     public void createVozilo_Success() {
+        VoziloRequestDTO voziloRequestDTO = VoziloRequestDTO.builder()
+                .registracija("bg334")
+                .kapacitet(32)
+                .brojRedova(4)
+                .brojKolona(8)
+                .kompanijaId(1L)
+                .build();
         Vozilo vozilo = builderVozilo();
+        when(voziloMapper.toEntity(voziloRequestDTO)).thenReturn(vozilo);
+        when(kompanijaRepository.findById(1L)).thenReturn(Optional.of(savedKompanija));
         when(voziloRepository.save(vozilo)).thenReturn(vozilo);
-        Vozilo saved = voziloService.createVozilo(vozilo);
+        when(voziloMapper.toResponse(vozilo)).thenReturn(new VoziloResponseDTO());
+        VoziloResponseDTO saved = voziloService.createVozilo(voziloRequestDTO);
         Assertions.assertThat(saved).isNotNull();
     }
 
     @Test
     public void createVozilo_ThrowsExceptionWhenRegistracijaExists() {
-        Vozilo vozilo = builderVozilo();
-        when(voziloRepository.existsByRegistracija(vozilo.getRegistracija())).thenReturn(true);
-        Assertions.assertThatThrownBy(() -> voziloService.createVozilo(vozilo))
+        VoziloRequestDTO voziloRequestDTO = VoziloRequestDTO.builder()
+                .registracija("bg334")
+                .kapacitet(32)
+                .brojRedova(4)
+                .brojKolona(8)
+                .kompanijaId(1L)
+                .build();
+        when(voziloRepository.existsByRegistracija(voziloRequestDTO.getRegistracija())).thenReturn(true);
+        Assertions.assertThatThrownBy(() -> voziloService.createVozilo(voziloRequestDTO))
                 .isInstanceOf(EntityAlreadyExistsException.class)
                 .hasMessage("Vozilo sa ovom registracijom vec postoji");
     }
@@ -84,7 +110,8 @@ public class VoziloServiceTest {
         Vozilo vozilo = builderVozilo();
         when(voziloRepository.findById(1L)).thenReturn(Optional.of(vozilo));
         when(voziloRepository.save(vozilo)).thenReturn(vozilo);
-        Vozilo updated = voziloService.updateVozilo(1L, 40, "bgr3f", 10, 4);
+        when(voziloMapper.toResponse(vozilo)).thenReturn(new VoziloResponseDTO());
+        VoziloResponseDTO updated = voziloService.updateVozilo(1L, 40, "bgr3f", 10, 4);
         Assertions.assertThat(updated).isNotNull();
     }
 
