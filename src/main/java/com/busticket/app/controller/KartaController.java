79f1a4414303cd01dcad37847c5b5a9cad56fb53
teodora <teamikic18@gmail.com>
2026-04-14 12:57:55 +0200

    fix: refactored Karta and Putovanje  controller to use ResponseEntity and dtos

--- src/main/java/com/busticket/app/controller/KartaController.java
+++ src/main/java/com/busticket/app/controller/KartaController.java
@@ -1,8 +1,10 @@
 package com.busticket.app.controller;
 
-import com.busticket.app.model.entity.Karta;
+import com.busticket.app.model.dto.RequestDTOs.KartaRequestDTO;
+import com.busticket.app.model.dto.ResponseDTOs.KartaResponseDTO;
 import com.busticket.app.service.KartaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,27 +16,28 @@ public class KartaController {
     private final KartaService kartaService;
 
     @GetMapping("/api/karte/{id}")
-    public Karta getKartaById(@PathVariable Long id) {
-        return kartaService.getKartaById(id);
+    public ResponseEntity<KartaResponseDTO> getKartaById(@PathVariable Long id) {
+        return ResponseEntity.ok(kartaService.getKartaById(id));
     }
 
     @GetMapping("/api/karte/putovanje/{id}")
-    public List<Karta> getKarteForPutovanje(@PathVariable Long id) {
-        return kartaService.getAllKarteForPutovanje(id);
+    public ResponseEntity<List<KartaResponseDTO>> getKarteForPutovanje(@PathVariable Long id) {
+        return ResponseEntity.ok(kartaService.getAllKarteForPutovanje(id));
     }
 
     @PostMapping("/api/karte")
-    public Karta createKarta(@RequestBody Karta karta) {
-        return kartaService.createKarta(karta);
+    public ResponseEntity<KartaResponseDTO> createKarta(@RequestBody KartaRequestDTO karta) {
+        return ResponseEntity.status(201).body(kartaService.createKarta(karta));
     }
 
     @PutMapping("/api/karte/{id}")
-    public Karta updateKarta(@RequestBody Karta karta, @PathVariable Long id) {
-        return kartaService.updateKarta(id, karta.getBrojSedista(), karta.getTip());
+    public ResponseEntity<KartaResponseDTO> updateKarta(@RequestBody KartaRequestDTO karta, @PathVariable Long id) {
+        return ResponseEntity.ok(kartaService.updateKarta(id, karta.getBrojSedista(), karta.getTip()));
     }
 
     @DeleteMapping("/api/karte/{id}")
-    public void deleteKarta(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteKarta(@PathVariable Long id) {
         kartaService.deleteKarta(id);
+        return ResponseEntity.noContent().build();
     }
 }
