965797b99a3c84af86ab8adb951203bc7087599e
teodora-mikic <tm20220196@student.fon.bg.ac.rs>
2026-07-01 16:40:44 +0200

    feat: added jwt auth

---
+++ src/main/java/com/busticket/app/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,8 @@
+package com.busticket.app.exceptions;
+
+public class InvalidCredentialsException extends RuntimeException{
+    
+    public InvalidCredentialsException(String message){
+        super(message);
+    }
+}
