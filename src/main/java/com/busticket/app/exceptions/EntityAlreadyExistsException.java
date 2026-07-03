549b7ad0f99fb486cfb8168ab1adc6dc35b85c2a
teodora-mikic <tm20220196@student.fon.bg.ac.rs>
2026-07-03 12:41:11 +0200

    fix

--- src/main/java/com/busticket/app/exceptions/EntityAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.busticket.app.exceptions;
-
-public class EntityAlreadyExistsException extends RuntimeException {
-
-    public EntityAlreadyExistsException(String message){
-        super(message);
-    }
-}
--- src/main/java/com/busticket/app/model/entity/enums/NacinPlacanja.java
+++
@@ -1,5 +0,0 @@
-package com.busticket.app.model.entity.enums;
-
-public enum NacinPlacanja {
-    KARTICA, GOTOVINA, ONLINE
-}
